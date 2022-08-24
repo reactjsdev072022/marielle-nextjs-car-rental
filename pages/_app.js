42319162855a81643603948b42fcb4d28908a7e6
Marielle Saman <samanmarielle03@gmail.com>
2022-08-24 14:54:28 +0800

    car-rental-app commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>,
+  </>
+  )
 }
 
 export default MyApp
